e9fdfb25b0b02cf1e64784ac1f1350d3917d8463
DiKachura <90569009+DiKachura@users.noreply.github.com>
2023-03-02 22:49:55 +0300

    Update dichotomy.py

--- dichotomy.py
+++ dichotomy.py
@@ -8,18 +8,19 @@ def main():
     eps = 0.2
     y = (a + b - eps) / 2
     z = (a + b + eps)/2
-    print(f'Промежуток: [{a},{b}]')
+    print(f'Промежуток: [{a},{b}]\n')
     while (abs(b - a) > 2 * eps):
         print('Сравниваем f(yk) с f(zk)')
+        print(f'f(y{k}) = {f(y)}, f(z{k}) = {f(z)} =>')
         if (f(y) <= f(z)):
-            print('f(yk) <= f(zk)')
+            print(f'f(y{k}) <= f(z{k})')
             print(f'Таким образом, a{k+1} = a{k} = {a}; b{k+1} = z{k} = {z}')
             b = z
         else:
-            print('f(yk) >= f(zk)')
+            print('f(y{k}) > f(z{k})')
             print(f'Таким образом, a{k + 1} = y{k} = {y}; b{k + 1} = b{k} = {b}')
             a = y
-        print(f'Промежуток:[{a},{b}]')
+        print(f'Промежуток:[{a},{b}]\n')
         y = (a + b - eps) / 2
         z = (a + b +eps)/2
         k += 1
