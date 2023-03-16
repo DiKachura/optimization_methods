ac4a222914bcf67df087bc2cd0d3f72a7bc59dd8
DiKachura <90569009+DiKachura@users.noreply.github.com>
2023-03-16 21:04:51 +0300

    Update the_golden_ratio_method.py

--- the_golden_ratio_method.py
+++ the_golden_ratio_method.py
@@ -4,8 +4,7 @@ from math import sqrt
 def f(x):
     return x**2+6*x+13
 
-# Метод золотого сечения
-#def Golden_ratio(f, a, b, eps):
+
 def main():
     print('Метод золотого сечения')
     a = -6
@@ -13,26 +12,38 @@ def main():
     eps = 0.5
     k = 0
     phi = (3 - sqrt(5)) / 2  # константа золотого сечения
-    y = a+phi*(b-a)
-    z = a+b-y
+    y = a + phi * (b - a)
+    z = a + b - y
+    fy = f(y)
+    fz = f(z)
     print(f'Промежуток: [{a},{b}]\n')
-    while(abs(b-a)>2*eps):
+    l = 2 * eps
+    ind = True
+    while(abs(b-a)>l):
         print('Сравниваем f(yk) с f(zk)')
         print(f'f(y{k}) = {f(y)}, f(z{k}) = {f(z)} =>')
-        if f(y) <= f(z):
+        if fy <= fz:
             y0 = y
             print(f'f(y{k}) <= f(z{k})')
             print(f'Таким образом, a{k + 1} = a{k} = {a}; b{k + 1} = z{k} = {z}; y{k+1} = a{k+1} + b{k+1} - y{k} = {a+b-y0}; z{k+1} = y{k} = {y0}')
-            b=z
-            y=a+b-y0
-            z=y0
+            b = z
+            z = y0
+            fz = fy
+            ind = True
         else:
             print('f(y{k}) > f(z{k})')
             print(f'Таким образом, a{k + 1} = y{k} = {y}; b{k + 1} = b{k} = {b}; y{k+1} = z{k} = {z}; z{k+1} = a{k+1} + b{k+1} - z{k} = {a+b-y}')
-            a=y
-            y=z
-            z=a+b-y
+            a = y
+            y = z
+            fy = fz
+            ind = False
+        if ind == True:
+            y = a + b - z
+            fy = f(y)
+        else:
+            z = a + b - y
+            fz = f(z)
         print(f'Промежуток:[{a},{b}]\n')
-        k+=1
+        k += 1
     print(f'x* = {(a + b) / 2} +(-) {(b - a) / 2}')
 main()
