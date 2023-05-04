f900d13b849b8d3dd37cb56420043e47f45fcb44
DiKachura <90569009+DiKachura@users.noreply.github.com>
2023-05-04 22:08:53 +0300

    Update gradient_descent_method.py

--- gradient_descent_method.py
+++ gradient_descent_method.py
@@ -1,79 +1,9 @@
-"""import numpy as np
-from fibonacci import fibonacci
-from swann_method import swann_method
-'''
-f(x) = x1^2 + 8x2^2 - x1x2 + x1
-x0 = (1,5; 0,1)
-'''
-def differentiable_function(x1, x2):
-    return x1**2 + 8 * x2**2 - x1 * x2 + x1
-
-def derivative_x2(x, eps):
-    return (differentiable_function(x[0], x[1] + eps) - differentiable_function(x[0], x[1])) / eps
-
-def derivative_x1(x, eps):
-    return (differentiable_function(x[0] + eps, x[1]) - differentiable_function(x[0], x[1])) / eps
-
-def grad_f(x):
-    return np.array([derivative_x1(x, eps=1e-10), derivative_x2(x, eps=1e-10)])
-
-def next_x(x, t):
-    temp = grad_f(x)
-    return x[0] - t*temp[0], x[1] - t*temp[1]
-
-
-def ft(x, xk, tk):
-    return differentiable_function(x)
-
-def gradient(x, eps1, eps2, M):
-    grad = grad_f(x)
-    k = 0
-    if np.linalg.norm(grad) < eps1:
-        print(f'x* = {x}')
-    else:
-        if k >= M:
-            print(f'x* = {x}')
-        else:
-            t = swann_method(x, 1)
-    '''grad = grad_f(x)
-    eps = 1e-15
-    k = 0
-    resolved = False
-    a, b = swann_method(x, 1)
-    while k < M and not resolved:
-        k += 1
-        if eps1 > np.linalg.norm(grad):
-            print(f'если || grad(fx{k})|| < eps1, т.е. {np.linalg.norm(grad)} < {eps1}')
-            resolved = True
-        else:
-            print(f'если || grad(fx{k})|| >= eps1, т.е. {np.linalg.norm(grad)} >= {eps1}')
-            t = fibonacci(differentiable_function(x[0], x[1], ))
-            newx, newy = next_x(x, t)
-            while not differentiable_function(newx, newy) - differentiable_function(x[0], x[1]) < - eps * np.linalg.norm(grad) ** 2:
-                print(f'если f(x{k+1}) - f(x{k}) < -eps * ||grad(f(x{k}))^2, т.е. {differentiable_function(newx, newy) - differentiable_function(x[0], x[1])} < {- eps * np.linalg.norm(grad) ** 2} и')
-                x[0], x[1] = newx, newy
-                print(f'x{k} = {x[0]}, x{k} = {x[1]}')
-                t/=2
-                newx, newy = next_x(x, t)
-                print(f'x{k+1} = {newx}, x{k+1} = {newy}')
-            if np.sqrt((x[0] - newx) ** 2 + (x[1] - newy) ** 2) < eps2 and np.fabs(differentiable_function(newx, newy) - differentiable_function(x[0], x[1])) < eps2:
-                print(f'||x{k+1} - x{k}|| < eps2 и |f(x{k+1}) - f(x{k})| < eps2, т.е. {np.sqrt((x[0] - newx) ** 2 + (x[1] - newy) ** 2)} < {eps2} и {np.fabs(differentiable_function(newx, newy) - differentiable_function(x[0], x[1]))} < {eps2}')
-                resolved = True
-            k+=1
-            x[0], x[1] = newx, newy
-            print(f'x1 = {x[0]}, x2 = {x[1]},\nf(x) =  {differentiable_function(x[0], x[1])}')'''
-
-gradient([1.5, 0.1], 0.1, 0.15, 10)"""
-
-
-
-
 import numpy as np
 from math import *
 
 def differentiable_function(x1, x2):
-    #return x1**2 + 8 * x2**2 - x1 * x2 + x1
-    return 2*x1**2+x1*x2+x2**2
+    return x1**2 + 8 * x2**2 - x1 * x2 + x1
+    #return 2*x1**2+x1*x2+x2**2
 def derivative_x2(x, eps):
     return (differentiable_function(x[0], x[1] + eps) - differentiable_function(x[0], x[1])) / eps
 
@@ -81,7 +11,7 @@ def derivative_x1(x, eps):
     return (differentiable_function(x[0] + eps, x[1]) - differentiable_function(x[0], x[1])) / eps
 
 def grad_f(x):
-    return np.array([derivative_x1(x, eps=1e-10), derivative_x2(x, eps=1e-10)])
+    return np.array([derivative_x1(x, eps=1e-10), derivative_x2(x, eps=1e-10)], dtype=np.longdouble)
 
 
 def swann_method(f, x0, t):
@@ -92,13 +22,13 @@ def swann_method(f, x0, t):
     x1 = f(x0-t)
     x2 = f(x0)
     x3 = f(x0 + t)
-    if (x1>=x2).all() and (x2<=x3).all():
+    if (x1>=x2) and (x2<=x3):
         a = x0 - t
         b = x0 + t
         flag = False
-    elif (x1<x2).all() and (x2>x3).all() or a!=0 and b!=0:
+    elif (x1<x2) and (x2>x3) or a!=0 and b!=0:
         flag = False
-    if (x1>=x2).all() and (x2>x3).all():
+    if (x1>=x2) and (x2>x3):
         delta = t
         a = x0
     else:
@@ -108,7 +38,7 @@ def swann_method(f, x0, t):
         k+=1
         x_prev = x0
         x0 += pow(2, k-1) * delta
-        if (f(x0)<f(x_prev)).all():
+        if (f(x0)<f(x_prev)):
             if delta > 0:
                 a = x_prev
             else:
@@ -130,7 +60,7 @@ def the_golden_ratio(f, a, b, eps):
     fz = f(z)
     l = 2 * eps
     while(abs(b-a)>l):
-        if (fy <= fz).all():
+        if (fy <= fz):
             b = z
             ind = True
         else:
@@ -166,9 +96,10 @@ def gradient_descent(x0, eps, eps1, eps2, M):
         def f(alpha):
             x_new = [x[0] - alpha * grad[0], x[1] - alpha * grad[1]]
             #print(f'x_new = {x_new}')
-            return grad_f(x_new)
+            #print(differentiable_function(x_new[0], x_new[1]))
+            return differentiable_function(x_new[0], x_new[1])
 
-        a, b = swann_method(f, 0, 1)
+        a, b = swann_method(f, -1, 1)
         print(f'a = {a}, b = {b}')
         tk = the_golden_ratio(f, a, b, eps)
         print(f'tk = {tk}')
@@ -176,6 +107,7 @@ def gradient_descent(x0, eps, eps1, eps2, M):
         print(f'x_next = {x_next}')
         if np.linalg.norm(x_next - x) < eps2 and abs(differentiable_function(x_next[0], x_next[1]) - differentiable_function(x[0], x[1])) < eps2:
             print(f'последняя проверка')
+            x = x_next
             break
         x = x_next
         print(f'x = {x}')
